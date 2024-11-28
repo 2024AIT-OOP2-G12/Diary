d69d28836d238b0a5e0bc96ad3f2afc3a824d9ca
yamamotokitta <yamashitarei1@gmail.com>
2024-11-28 16:24:10 +0900

    kousinn

--- diaries/KittaDiary.py
+++ diaries/KittaDiary.py
@@ -2,10 +2,10 @@ from diaries.AbstractDiary import AbstractDiary
 class KittaDiary(AbstractDiary):
     def get_date(self):
         return "2024-11-28"
-def get_summary(self):
-    return
-    """今日はオブジェクト指向プログラミング演習2のグループワーク演習だった。
-    初めてのことが多く、理解するのが難しかった。"""
+    def get_summary(self):
+        return """
+            今日はオブジェクト指向プログラミング演習2のグループワーク演習だった。
+            初めてのことが多く、理解するのが難しかった。"""
 
-def get_author(self):
-    return "Kitta"
+    def get_author(self):
+        return "Kitta"
