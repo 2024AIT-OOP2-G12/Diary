d7dc895f69017f0901f4db0f510230dc96dcb595
kei928 <nayutan2080@gmail.com>
2024-11-28 15:26:43 +0900

    丹羽を書いた

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.NiwaDiary import NiwaDiary
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(),NiwaDiary()]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
     print(d.get_summary())
     print(d.get_author())
+    
     print()
